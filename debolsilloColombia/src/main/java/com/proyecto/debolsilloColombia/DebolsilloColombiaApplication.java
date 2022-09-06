43fe4106ac142f70f6707e7f92568abe3fccce2a
daniel steban guarin criollo <daniel11544@hotmail.com>
2022-09-06 16:05:40 -0500

    subir parte de sergio

---
+++ debolsilloColombia/src/main/java/com/proyecto/debolsilloColombia/DebolsilloColombiaApplication.java
@@ -0,0 +1,12 @@
+package com.proyecto.debolsilloColombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebolsilloColombiaApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(DebolsilloColombiaApplication.class, args);
+	}
+
+}
---
+++ debolsilloColombia/src/main/java/com/proyecto/debolsilloColombia/entidades/Empleado.java
@@ -0,0 +1,4 @@
+package com.proyecto.debolsilloColombia.entidades;
+
+public class Empleado {
+}
